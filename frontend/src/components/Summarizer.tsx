33064232942512d5f5d5eda7607f867a6f9e32c4
senesh meedum hansana <96445969+seneshMH@users.noreply.github.com>
2025-11-25 17:34:09 +0530

    fixed ui bug

--- frontend/src/components/Summarizer.tsx
+++ frontend/src/components/Summarizer.tsx
@@ -61,7 +61,7 @@ export default function Summarizer() {
                             <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
                                 <path fillRule="evenodd" d="M4 4a2 2 0 012-2h4.586A2 2 0 0112 2.586L15.414 6A2 2 0 0116 7.414V16a2 2 0 01-2 2H6a2 2 0 01-2-2V4z" clipRule="evenodd" />
                             </svg>
-                            <h2 className="text-xs font-semibold uppercase tracking-wider">
+                            <h2 className="text-base font-semibold uppercase tracking-wider">
                                 Original Text
                             </h2>
                         </div>
